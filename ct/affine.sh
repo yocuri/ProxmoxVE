18598b06216effe5708ba92d91b3fd2c2609322d
Lauren X Reluna <laurenvandebrake@my.unt.edu>
2026-09-04 18:21:36 -0500

    Update default URL for community scripts

--- ct/affine.sh
+++ ct/affine.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-_CS_DEFAULT_URL="https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main"
+_CS_DEFAULT_URL="https://raw.githubusercontent.com/yocuri/ProxmoxVE/main"
 _cs_boot="${COMMUNITY_SCRIPTS_CORE_DIR:-$(dirname "${BASH_SOURCE[0]}")/../../core}/core/build.func"
 source "$_cs_boot" 2>/dev/null || source <(curl -fsSL "${COMMUNITY_SCRIPTS_CORE_URL:-https://raw.githubusercontent.com/community-scripts/core/main}/core/build.func")
 # Copyright (c) 2021-2026 community-scripts ORG
